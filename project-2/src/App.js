977dda257f66f836ad7e46f672e0f0b4df717307
Azaz Ali <aliazaz7296@gmail.com>
2023-03-25 20:55:07 +0530

    done

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ContactCard from "./ContactCard";
+// Here we just declared a component
 
+// A component in react is just a function that returns some data
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=20")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results.map((result, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={result.picture.large}
+            name={result.name.first}
+            email={result.email}
+            age={result.dob.age}
+          />
+        );
+      })}
     </div>
   );
 }
